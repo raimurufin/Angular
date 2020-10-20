8312e37c1f8d7d801241e8e31888194106c42552
raimurufin <r.rufinlarrain98@gmail.com>
2020-10-20 10:13:29 -0300

    Pikme
    Integración de la app Pikme al Repositorio

---
+++ pikme1/src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { AuthService } from './../auth.service';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  constructor(private authService: AuthService) {
+  }
+
+  login() {
+
+    this.authService.login();
+  }
+
+}
